03232eaffd9f31540595ddd79bb43d6a2e7e7565
Barry Carlyon <barry@barrycarlyon.co.uk>
2012-11-08 21:29:59 +0000

    put the clock in a div so it is nicely spaced out

--- public_html/modules/clock/clock.php
+++ public_html/modules/clock/clock.php
@@ -9,10 +9,13 @@ class clock extends module
 		$header = '
 <script type="text/javascript">
 	jQuery(document).ready(function() {
-		swfobject.embedSWF("' . DASHBOARD_URL_MODULES . 'clock/tiny.swf", "' . $this->id . '_content", "200", "200", "9.0.0", "", {}, {wmode: "transparent"});
+		swfobject.embedSWF("' . DASHBOARD_URL_MODULES . 'clock/tiny.swf", "' . $this->id . '_clock", "200", "200", "9.0.0", "", {}, {wmode: "transparent"});
 	});
 </script>
 ';
 		return $header;
+	}
+	public function content() {
+		return '<div id="' . $this->id . '_clock"></div>';
 	}
 }
