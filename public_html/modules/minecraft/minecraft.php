316ab7e746d7e9e01c2f21cb2dcd0218804b81ee
Barry Carlyon <barry@barrycarlyon.co.uk>
2012-11-20 22:27:33 +0000

    use this script as failback instead http://forums.bukkit.org/threads/solved-minecraft-server-list-ping-php-script-for-1-4.108096/#post-1395553

--- public_html/modules/minecraft/minecraft.php
+++ public_html/modules/minecraft/minecraft.php
@@ -17,8 +17,12 @@ class minecraft extends module
 		$query = new GSQuery();
 		$query->SetProtocol("GameSpy4");
 
+//		$server = 'barrycarlyon.co.uk';
+		$server = 'play.phantomcraft.net';
+		$port = '25565';
+
 //		$query->SetIpPort("216.189.8.183:2302");
-		$query->SetIpPort("barrycarlyon.co.uk:25565");
+		$query->SetIpPort($server .':' . $port);
 //		$query->SetIpPort("play.phantomcraft.net:25565");
 
 		$query->SetRequestData(array("FullInfo"));
@@ -36,6 +40,48 @@ class minecraft extends module
 			echo 'fail back';
 			// blanked
 			// switch to alt method
+
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); //Create the socket
+$connected = socket_connect($socket, $server, $port); //Try and connect using the info provided above
+ 
+if (!$connected)
+    die("Could not connect to server."); //No connection could be established
+ 
+socket_send($socket, "\xFE\x01", 2, 0); //Send the server list ping request (two bytes)
+$retVal = socket_recv($socket, $data, 1024, 0); //Get the info and store it in $data
+socket_close($socket); //Close socket
+ 
+if ($retVal != false && substr($data, 0, 1) == "\xFF") //Ensure we're getting a kick message as expected
+{
+    $data = substr($data, 9); //Remove packet, length and starting characters
+    $data = explode("\x00\x00", $data); //0000 separated info
+
+    print_r($data);
+/*
+    $protocolVersion = $data[0]; //Get it all into separate variables
+    $serverVersion = $data[1];
+    $motd = $data[2];
+    $playersOnline = $data[3];
+    $playersMax = $data[4];
+*/
+
+			$existing = json_decode(file_get_contents(DASHBOARD_CACHE_PATH . 'minecraft.json'), TRUE);
+			$existing['Details']['version'] = $data[1];
+			$existing['Details']['hostname'] = $data[2];
+			$existing['Details']['numplayers'] = $data[3];
+			$existing['Details']['maxplayers'] = $data[4];
+
+
+			$fp = fopen(DASHBOARD_CACHE_PATH . 'minecraft.json', 'w');
+			fwrite($fp, json_encode($existing));
+			fclose($fp);
+}
+else
+{
+//    die("Couldn't get expected data"); //Either retVal was false or we didn't get a kick message
+}
+return;
 
 			// create
 			$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -76,10 +122,19 @@ class minecraft extends module
 				// server DOWN
 				exit;
 			}
+
+echo '----' . "\n";
+echo utf8_decode($string);
+echo '----' . "\n";
+
 			$data = explode("\xa7", $string);
 
+			print_r($data);
+
 			$existing = json_decode(file_get_contents(DASHBOARD_CACHE_PATH . 'minecraft.json'), TRUE);
+			$existing['Details']['hostname'] = $data[0];
 			$existing['Details']['numplayers'] = $data[1];
+			$existing['Details']['maxplayers'] = $data[2];
 /*
 			$server_description = $data[0];
 			$players_on = $data[1];
@@ -112,13 +167,17 @@ class minecraft extends module
 		// process
 		$html = '<table style="width: 100%;">';
 		$html .= '<tr><td>HostName</td><td>' . $data['Details']['hostname'] . '</td></tr>';
-		$html .= '<tr><td>Version</td><td>' . $data['Details']['version'] . '</td></tr>';
-		$html .= '<tr><td>Players</td><td>' . $data['Details']['numplayers'] . '/' . $data['Details']['maxplayers'] . '</td></tr>';
-		$html .= '<tr><td></td><td><ul>';
-		foreach ($data['Players'] as $player) {
-			$html .= '<li>' . $player['player'] . '</li>';
+		if (isset($data['Details']['version'])) {
+			$html .= '<tr><td>Version</td><td>' . $data['Details']['version'] . '</td></tr>';
 		}
-		$html .= '</ul></td></tr>';
+		$html .= '<tr><td>Players</td><td>' . $data['Details']['numplayers'] . '/' . $data['Details']['maxplayers'] . '</td></tr>';
+		if (isset($data['Players'])) {
+			$html .= '<tr><td></td><td><ul>';
+			foreach ($data['Players'] as $player) {
+				$html .= '<li>' . $player['player'] . '</li>';
+			}
+			$html .= '</ul></td></tr>';
+		}
 		$html .= '</table>';
 		return $html;
 	}
